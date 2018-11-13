2ebbe000acaa56cd74069d67486c6001421e5305
kbbushman <k.bushman@yahoo.com>
2018-11-13 08:52:03 -0800

    Adds redux store

---
+++ src/components/Shelf.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+
+class Shelf extends Component {
+  state = {
+    shelfItems: [
+      'Bananas',
+      'Frozen Pizza',
+      'Flamin Hot Cheetos',
+      'Arugula',
+    ]
+  }
+
+  render() {
+    const shelfItems = this.state.shelfItems.map((item, id) => {
+      return (
+        <li key={id}>
+          { item }
+          <button onClick={() => this.props.addItem(item)}> + </button>
+        </li>
+      )
+    });
+
+    return (
+      <div>
+        <h2>Store Inventory</h2>
+        <ul>{ shelfItems }</ul>
+      </div>
+    )
+  }
+}
+
+export default Shelf;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import { Provider } from 'react-redux';
+import Store from './Store';
+import App from './components/App';
+
+import './stylesheets/index.css';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const StoreInstance = Store();
+
+ReactDOM.render(
+  <Provider store={StoreInstance}>
+    <App />
+  </Provider>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
